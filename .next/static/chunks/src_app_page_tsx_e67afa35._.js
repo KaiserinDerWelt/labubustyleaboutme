c39579b5b8fa5a60ed0b63df29ec4edb17fc6c92
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2025-12-04 16:02:34 -0600

    chore: :wastebasket: Clean up branch

--- .next/static/chunks/src_app_page_tsx_e67afa35._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_components_home_education-skills_index_tsx_d439a985._.js"
-],
-    source: "dynamic"
-});
